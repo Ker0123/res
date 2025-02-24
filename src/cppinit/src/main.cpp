a54f135b30cad82433bdb0d8d0a972ca6078cc00
Ker_0123 <ker_0123@163.com>
2025-02-24 11:18:14 +0800

    cppinit的修正, 使用base64转换为示例

--- src/cppinit/src/main.cpp
+++ src/cppinit/src/main.cpp
@@ -11,28 +11,29 @@
 
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 using namespace std;
 
-#include "myclass.h"
+#include "args.h"
 
 int main(int argc, char const *argv[])
 {
+    #ifdef _WIN32
+    system("C:\\Windows\\system32\\chcp 65001 > NUL"); // 解决Windows下中文乱码问题
+    #endif
+
     cout << "================================================================" << endl;
     cout << "Last build: \t" << __DATE__ << " " << __TIME__ << endl;
-    cout << "v0.0.1: \t" << "2025-01-24 11:00:00 " << "第一个小版本" << endl;
-    cout << "v0.0.0: \t" << "2025-01-24 10:00:00 " << "搭建好项目环境" << endl;
+    cout << "v0.0.1: \t" << "2025-01-24 11:00 " << "第一个小版本" << endl;
+    cout << "v0.0.0: \t" << "2025-01-24 10:00 " << "搭建好项目环境" << endl;
     cout << "----* START *---------------------------------------------------" << endl;
 
-    // 获取命令行参数(如果没有参数, 则默认为"null")
-    string iStr = "null";
-    if(argc > 1)
-    {
-        iStr = string(argv[1]);
-    }
+    // 解析命令行参数
+    
 
-    // 存储到类并打印
-    MyClass c(iStr);
-    c.Print();
 
     cout << "----* END *-----------------------------------------------------" << endl;
     return 0;
